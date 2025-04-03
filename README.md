b0fbe1369eddc7fc1661d2b6755407bd4cb5d6f2
nlippincott <nl@acm.org>
2025-04-03 09:07:34 -0400

    Updated exercise for Sp25 semester

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # A Linked List Template Class
 
-Starter code for an exercise introducing linked lists.
+Starter code for an in-class exercise introducing linked lists.
+
+A linked list is a data structure that stores objects as an ordered list.
+Typical operations of a linked list include ability to append an object to
+the end of the list, prepend an object at the beginning of the list, insertion
+of objects in the list, access to objects by their position in the list,
+and navigation of objects sequentially.
+
+For this exercise, the following operations will be implemented:
+
+- Structure of a linked list node
+- Member variables to maintiain the list
+  - Pointers to head and tail nodes
+  - Count of objects in the list
+- Initialization of the list (constructor)
+- Get the list's size (count of objects in the list)
+- Appending and prepending objects to the list
+- Print all objects in the list (navigation)
+- Accessing an object by its index (operator[])
+  - Replace object by its index
+- Finding an object's index
+- Inserting an object at some index
+- Removing an object by its index
+- Removing all objects from the list
+- Implement the destructor
+- Implement a copy constructor
+- Implement the assignment operator
--- list.h
+++ list.h
@@ -1,23 +1,38 @@
 #pragma once
 
-// A single node in the linked list.
-template <typename T>
-class ListNode {
-public:
-
-};
+#include <cstddef>
+#include <iostream>
 
 // A linked list of objects.
 template <typename T>
 class List {
 protected:
 
+	// A node in the linked list, contains the object stored in the node
+	// and a pointer to the next node in the list.
+	class Node {
+	};
+
+	// Pointers to head and tail nodes
+
+	// Current list size
+
 public:
+
+	// Default Constructor
 	List () { }
+
+	// Copy Constructor
+	List (const List<T>& list);
+
+	// Assignment
+	List<T>& operator= (const List<T>& list);
+
+	// Destructor
 	~List () { }
 
 	// Adds one object to the end of the list
-	void append (const T& item);
+	void append (const T& obj);
 
 	// Clears the list, making it empty
 	void clear ();
@@ -28,15 +43,21 @@ public:
 	// Inserts an object prior to the object at the given index
 	void insert (size_t index, const T& obj);
 
+	// Adds one object to the start of the list
+	void prepend (const T& obj);
+
+	// Navigate the list, printing each object with its index
+	void print () const;
+
 	// Removes the object at the given index
 	void remove (size_t index);
-
-	// Replace the object at the given index with the given object
-	void replace (size_t index, const T& obj);
 
 	// Return the number of objects in the list
 	size_t size () const;
 
 	// Return the object at the given index
+	T& operator[] (size_t index);
+
+	// Return the object at the given index (constant version)
 	T operator[] (size_t index) const;
 };
